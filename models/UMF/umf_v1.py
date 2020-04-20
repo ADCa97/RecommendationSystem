ba5a2a19a43f360d9a7fc2359db8eb714cc9936d
gzc <624040754@qq.com>
2020-04-20 17:50:03 +0800

    umf_v1.py修改，添加打印参数，计算总RMSE功能

--- models/UMF/umf_v1.py
+++ models/UMF/umf_v1.py
@@ -11,14 +11,15 @@ def parse_args():
     parser.add_argument('--src', type = str, default = '../../data/dataset_1/Office_Products')
     parser.add_argument('--dst', type = str, default = '../../data/dataset_1/Movies_and_TV')
     parser.add_argument('--share', type = str, default = '../../data/dataset_1/share_info.csv')
-    parser.add_argument('--learning_rate', type = float, default = 0.001)
-    parser.add_argument('--reg_rate', nargs = '?', default = '[0.001, 0.001, 0.001]')
+    parser.add_argument('--learning_rate', type = float, default = 0.01)
+    parser.add_argument('--reg_rate', nargs = '?', default = '[0.001, 0.001, 0]')
     parser.add_argument('--epochs', type = int, default = 50)
     parser.add_argument('--batch_size', type = int, default = 128)
+    parser.add_argument('--num_factor', type = int, default = 50)
     return parser.parse_args()
     
 class UMF():
-    def __init__(self, num_user, src_num_item, dst_num_item, learning_rate = 0.001, reg_rate = [0, 0, 0], epochs = 50, batch_size = 128):
+    def __init__(self, num_user, src_num_item, dst_num_item, learning_rate = 0.01, reg_rate = [0.001, 0.001, 0.1], epochs = 50, batch_size = 128, num_factor = 50):
         self.num_user = num_user
         self.src_num_item = src_num_item
         self.dst_num_item = dst_num_item
@@ -26,18 +27,23 @@ class UMF():
         self.reg_rate = reg_rate
         self.epochs = epochs
         self.batch_size = batch_size
+        self.num_factor = num_factor
         print("UMF init")
-        pass 
-    def build_network(self, num_factor = 50):
+        print("learning_rate", self.learning_rate)
+        print("reg_rate", self.reg_rate)
+        print("num_factor", self.num_factor)
+
+        time.sleep(2) 
+    def build_network(self):
         self.shareuser = tf.placeholder(dtype = tf.int32, shape = [None], name = 'shareuser')
         self.src_itemid = tf.placeholder(dtype = tf.int32, shape = [None], name = 'src_itemid')
         self.src_ratings = tf.placeholder(dtype = tf.float32, shape = [None], name = 'src_ratings')
         self.dst_itemid = tf.placeholder(dtype = tf.int32, shape = [None], name = 'dst_itemid')
         self.dst_ratings = tf.placeholder(dtype = tf.float32, shape = [None], name = 'dst_ratings')
 
-        self.U = tf.Variable(tf.random_normal([self.num_user, num_factor], stddev = 0.01), name = 'ShareUser')
-        self.src_V = tf.Variable(tf.random_normal([self.src_num_item, num_factor], stddev = 0.01), name = 'Src_Item')
-        self.dst_V = tf.Variable(tf.random_normal([self.dst_num_item, num_factor], stddev = 0.01), name = 'Dst_Item')
+        self.U = tf.Variable(tf.random_normal([self.num_user, self.num_factor], stddev = 0.01), name = 'ShareUser')
+        self.src_V = tf.Variable(tf.random_normal([self.src_num_item, self.num_factor], stddev = 0.01), name = 'Src_Item')
+        self.dst_V = tf.Variable(tf.random_normal([self.dst_num_item, self.num_factor], stddev = 0.01), name = 'Dst_Item')
 
         shareuser_latent_factor = tf.nn.embedding_lookup(self.U, self.shareuser)
         src_item_latent_factor = tf.nn.embedding_lookup(self.src_V, self.src_itemid)
@@ -64,6 +70,7 @@ if __name__ == '__main__':
     reg_rate = eval(args.reg_rate)
     epochs = args.epochs
     batch_size = args.batch_size
+    num_factor = args.num_factor
 
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
@@ -75,7 +82,7 @@ if __name__ == '__main__':
     print(src_n_users, src_n_items, dst_n_users, dst_n_items, len(shareuser), type(shareuser))
     with tf.Session(config = config) as sess:
         model = None
-        model = UMF(src_n_users, src_n_items, dst_n_items, learning_rate, reg_rate, epochs, batch_size)
+        model = UMF(src_n_users, src_n_items, dst_n_items, learning_rate, reg_rate, epochs, batch_size, num_factor)
         if model is not None:
             model.build_network()
         optimizer = tf.train.AdamOptimizer(learning_rate = model.learning_rate).minimize(model.loss)
@@ -111,6 +118,8 @@ if __name__ == '__main__':
                 if i % 1000 == 0:
                     print("Cost = %.9f" %loss)
             if epoch % 10 == 0:
+                all_error = 0
+                all_test = 0
                 src_error = 0
                 src_set = list(src_test_matrix.keys())
                 for (u, i) in src_set:
@@ -118,6 +127,8 @@ if __name__ == '__main__':
                                                                                     model.src_itemid: [i]})
                     src_error += (float(src_test_matrix.get((u, i))) - src_pred_rating_test) ** 2
                 print("src_error = %.9f number = %04d RMSE = %.9f" %(src_error, len(src_set), np.sqrt(src_error / len(src_set))))
+                all_error += src_error
+                all_test += len(src_set)
 
                 dst_error = 0
                 dst_set = list(dst_test_matrix.keys())
@@ -125,4 +136,7 @@ if __name__ == '__main__':
                     dst_pred_rating_test = sess.run(model.dst_pred_rating, feed_dict = {model.shareuser: [u],
                                                                                     model.dst_itemid: [i]})
                     dst_error += (float(dst_test_matrix.get((u, i))) - dst_pred_rating_test) ** 2
-                print("src_error = %.9f number = %04d RMSE = %.9f" %(dst_error, len(dst_set), np.sqrt(dst_error / len(dst_set))))
+                print("dst_error = %.9f number = %04d RMSE = %.9f" %(dst_error, len(dst_set), np.sqrt(dst_error / len(dst_set))))
+                all_error += dst_error
+                all_test += len(dst_set)
+                print("all_error = %.9f number = %04d RMSE = %.9f" %(dst_error, all_test, np.sqrt(all_error / all_test)))

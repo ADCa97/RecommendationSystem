72c336d491bbb8e788d0075fcb71186bc334b89f
gzc <624040754@qq.com>
2020-04-25 11:26:36 +0800

    参数调优自动化测试脚本

---
+++ models/FinalModel/minrmse.py
@@ -0,0 +1,26 @@
+import pandas as pd 
+
+src_minrmse = []
+dst_minrmse = []
+csv_list = []
+all_minrmse = []
+
+for i in range(147):
+    datapath = './saver/Software_Video_Games/' + str(i) + '.csv'
+    df = pd.read_csv(datapath, names = ['cost', 'src_rmse', 'dst_rmse', 'all_rmse'])
+    df = df[(df.src_rmse < 2.061887) & (df.dst_rmse < 3.130049)]
+    if not df.empty:
+        print(datapath)
+        print(df)
+        csv_list.append(i)
+        all_minrmse.append(df['all_rmse'].min())
+print("Index#%d, all_minrmse#%f" %(csv_list[all_minrmse.index(min(all_minrmse))], min(all_minrmse)))
+'''
+    src_minrmse.append(df['src_rmse'].min())
+    dst_minrmse.append(df['dst_rmse'].min())
+    
+    
+print("Index#%d, src_minrmse#%f" %(src_minrmse.index(min(src_minrmse)), min(src_minrmse)))
+print("Index#%d, dst_minrmse#%f" %(dst_minrmse.index(min(dst_minrmse)), min(dst_minrmse)))
+
+'''
